27192dedb0de67ae3cde6831546dc10a89574651
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-12-30 09:16:21 +0530

    fs module path module

---
+++ fs and path modules/pathmodule.js
@@ -0,0 +1,9 @@
+const path = require('path')
+console.log(path)
+
+
+let mypath = "C:\\Users\\jyotisangwan\\Desktop\\pract-node\\fs and path modules\\abc.txt"
+console.log(path.extname(mypath))
+console.log(path.dirname(mypath))
+console.log(path.basename(mypath))
+console.log(path.join("C:/", "\\abc.txt"))
